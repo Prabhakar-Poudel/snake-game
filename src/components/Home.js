7d0140b54fca064351cc09eb3ef44822cd088cc5
Prabhakar <prabhakar.p@codeparva.in>
2019-02-10 10:03:13 +0530

    firebase login [WIP]

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react'
 import '../styles/Home.css'
 
 class Home extends Component {
+
+    handleSignIn = () => {
+        window.ui.start('#firebaseui-auth-container', window.uiConfig);
+    }
+
     render() {
         return (
             <div className="home-screen">
-                <button autoFocus onClick={this.props.handleStart}>Start</button>
-                <p>or</p>
-                <p>Just press Enter</p>
+                <button autoFocus onClick={this.props.handleStart}>Play</button>
+                <p></p>
+                <div className="sign-in" onClick={this.handleSignIn}>Sign In</div>
             </div>
         )
     }
